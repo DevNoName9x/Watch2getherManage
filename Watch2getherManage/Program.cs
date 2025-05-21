f84afa81ecd5a3f92e500fde59b47b1946c4b7bb
thienpa@fabbi.io <thienpa@fabbi.io>
2025-05-21 17:56:56 +0700

    update add DB

--- Watch2getherManage/Program.cs
+++ Watch2getherManage/Program.cs
@@ -19,7 +19,11 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
